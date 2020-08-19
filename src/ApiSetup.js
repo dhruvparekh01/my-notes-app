a75974695a0f8036c28f6edd2f757defb8659430
dhruvparekh01 <dhruvparekh01@gmail.com>
2020-08-19 14:52:15 -0700

    Change the API adress to custom https domain

--- src/ApiSetup.js
+++ src/ApiSetup.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // Create an axios instance and export it to be used for the entire application
 
 const request = axios.create({
-    baseURL: "http://notetakerapi-env.eba-gtdrqys2.us-east-2.elasticbeanstalk.com/"
+    baseURL: "https://www.notetakerapi.tk/"
 })
 
 // Attach an interceptor to get the value of jwt from session storage and use it as auth token
